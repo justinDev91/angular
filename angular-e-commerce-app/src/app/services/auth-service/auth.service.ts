9ab0e06090c0a575cda9de96cb6cf1bbc0ca50a9
justinDev91 <justin@sustainsoft.eu>
2023-05-24 18:17:21 +0200

    add projects

---
+++ angular-e-commerce-app/src/app/services/auth-service/auth.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import { LocalStorageService } from '../storage-service/local-storage.service';
+
+const BASIC_URL = environment["BASIC_URL"];
+export const AUTH_HEADER = "authorization";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient,
+    private storageService: LocalStorageService) { }
+
+  register(signupDTO: any): Observable<any> {
+    return this.http.post<[]>(BASIC_URL + "sign-up", signupDTO)
+  }
+
+  login(username: string, password: string): any {
+    console.log(username, password)
+    return this.http.post<[]>(BASIC_URL + 'authenticate',
+      { username, password },
+      { observe: 'response' })
+      .pipe(
+        tap(_ => this.log("User Authentication")),
+        map((res: HttpResponse<any>) => {
+          this.storageService.saveUserId(res.body.userId);
+          this.storageService.saveUserRole(res.body.role);
+          const tokenLength = res.headers.get(AUTH_HEADER).length;
+          const bearerToken = res.headers.get(AUTH_HEADER).substring(7, tokenLength);
+          this.storageService.saveToken(bearerToken);
+          return res;
+        })
+      )
+  }
+
+  log(message: string): void {
+    console.log(`User Auth Service: ${message}`)
+  }
+
+}
+
